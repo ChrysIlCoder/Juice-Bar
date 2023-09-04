a327cf209785ba0deeb1206068b04c1c0f6c898e
ChrysIlCoder <creepert11.creeper@gmail.com>
2023-09-04 20:33:16 +0200

    Timeline da fare in chi siamo

--- src/modules/chisiamo/chisiamoTimeline.jsx
+++ src/modules/chisiamo/chisiamoTimeline.jsx
@@ -2,6 +2,37 @@ import './chisiamo.css'
 
 export default function ChiSiamoTimeline(){
     return (
-        <span>ciapo</span>
+        <div>
+            <div className='milestones-container'>
+                <div className='milestone'>
+                    <span className='milestone-title'></span>
+                    <p className='milestone-desc'>
+
+                    </p>
+                </div>
+                <div className='milestone'>
+                    <span className='milestone-title'></span>
+                    <p className='milestone-desc'>
+
+                    </p>
+                </div>
+                <div className='milestone'>
+                    <span className='milestone-title'></span>
+                    <p className='milestone-desc'>
+
+                    </p>
+                </div>
+                <div className='milestone'>
+                    <span className='milestone-title'></span>
+                    <p className='milestone-desc'>
+
+                    </p>
+                </div>
+            </div>
+            <div className='milestone-points-container'>
+                
+            </div>
+            <div className='timeline-line'></div>
+        </div>
     )
 }
--- src/modules/footer.jsx
+++ src/modules/footer.jsx
@@ -5,14 +5,12 @@ export default function Footer(){
     return (
         <footer>
             <div className='footer-container'>
-                <span className='footer-logo'>Juice™.</span>
+                <span className='footer-logo'>Juicy</span>
                 <span className='footer-copyright'>Copyright © 2023, Juice Bar</span>
                 <ul className='footer-links-list'>
-                    <li className='footer-links underline-effect'><a href="/">Home</a></li>
-                    <li className='footer-links underline-effect'><a href="/menu">Menù</a></li>
-                    <li className='footer-links underline-effect'><a href="/trova">Trova un Bar</a></li>
-                    <li className='footer-links underline-effect'><a href="/chisiamo">Chi Siamo</a></li>
-                    <li className='footer-links underline-effect'><a href="/contact">Contattaci</a></li>
+                    <li className='footer-links'><a href="/" className='underline-effect'>Privacy</a></li>
+                    <li className='footer-links'><a href="/menu" className='underline-effect'>Termini & Condizioni</a></li>
+                    <li className='footer-links'><a href="/trova" className='underline-effect'>Policy Rimborsi</a></li>
                 </ul>
             </div>
         </footer>
