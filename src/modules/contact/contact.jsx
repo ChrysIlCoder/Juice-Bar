2619022ccbf2e1f391ca4a145c4289273822c462
ChrysIlCoder <creepert11.creeper@gmail.com>
2023-09-16 01:59:46 +0200

    Aggiunto npm start e finire contact

--- src/modules/contact/contact.jsx
+++ src/modules/contact/contact.jsx
@@ -1,9 +1,11 @@
+import ContactAlternativaSocial from "./contactAlternativa";
 import ContactSocials from "./contactSocials";
 
 export default function ContactContent(){
     return (
         <div className="contact-container">
             <ContactSocials />
+            <ContactAlternativaSocial />
         </div>
     )
 }
---
+++ src/modules/contact/contactAlternativa.jsx
@@ -0,0 +1,19 @@
+import './contact.css'
+
+export default function ContactAlternativaSocial(){
+    return (
+        <div className='contact-alternative-container'>
+            <div className='alternative-title'>
+
+            </div>
+            <div className='alternatives-container'>
+                <div className='alternative-email'>
+                    Oppure
+                </div>
+                <div className='alternative-phone'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
