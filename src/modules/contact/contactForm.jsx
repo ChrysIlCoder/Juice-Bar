b84d7291406da5f34b09c08d408148691d07f2f2
ChrysIlCoder <creepert11.creeper@gmail.com>
2023-09-23 10:27:34 +0200

    Finito Contact, Fare Job

--- src/modules/contact/contactForm.jsx
+++ src/modules/contact/contactForm.jsx
@@ -1,25 +1,52 @@
 import './contact.css'
+import { useState } from 'react'
 
 export default function ContactForm(){
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [email, setEmail] = useState('')
+    const [content, setContent] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        Email.send({
+            SecureToken: "794adbc2-32fa-4261-b0eb-1ec7b76cea5c",
+            To: 'chri.boffa@gmail.com',
+            From: email,
+            Subject: name + " " + surname,
+            Body: "Email: " + email + "<br>" +
+                  "Messaggio: " + content
+        }).then(
+            message => {
+                if (message.includes("Mailbox name not allowed")){
+                    alert(`L'Email "${email}" non esiste, perfavore usare un'email valida`)
+                }
+                else {
+                    alert("Inviata con successo")
+                }
+            }
+        );
+    }
+
     return (
         <div className='contact-form'>
-            <form action="">
+            <form method='GET' onSubmit={handleSubmit}>
                 <div className='form-inputs'>
                     <div className='form-name'>
                         <label>Nome</label>
-                        <input type="text" />
+                        <input type="text" placeholder='e.g "Jack"' required onChange={(e) => setName(e.target.value)}/>
                     </div>
                     <div className='form-surname'>
                         <label>Cognome</label>
-                        <input type="text" />
+                        <input type="text" placeholder='e.g "Jackson"' required onChange={(e) => setSurname(e.target.value)}/>
                     </div>
                     <div className='form-email'>
                         <label>Email</label>
-                        <input type="email" />
+                        <input type="email" placeholder='jack.jackson@gmail.com' required onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className='form-messagge'>
                         <label>Messaggio</label>
-                        <textarea cols="70" rows="10"></textarea>
+                        <textarea cols="70" rows="10" placeholder='Messaggio qui...' required onChange={(e) => setContent(e.target.value)}></textarea>
                     </div>
                 </div>
                 <input type="submit" value='Invia' className='form-submit'/>
--- src/modules/contact/contactInfo.jsx
+++ src/modules/contact/contactInfo.jsx
@@ -2,7 +2,7 @@ import './contact.css'
 import InfoItem from './contactInfoItem'
 import phoneIcon from '../../assets/PhoneIcon.png'
 import emailIcon from '../../assets/EmailIcon.png'
-import waypointIcon from '../../assets/PosizioneIcon.png'
+import instaIcon from '../../assets/InstaIcon.png'
 import facebookIcon from '../../assets/FacebookIcon.png'
 
 export default function ContactInfo(){
@@ -12,8 +12,8 @@ export default function ContactInfo(){
             <div className='info-socials-line'>
                 <InfoItem icon={phoneIcon} title={'Telefono'} info={'+1 605-475-6968'}/>
                 <InfoItem icon={emailIcon} title={'Email'} info={'christian.anzolin.gay@gmail.com'}/>
-                <InfoItem icon={waypointIcon} title={'Posizione'} info={'Via 4405 Neese Ln, Moss Point, MS 39563'}/>
-                <InfoItem icon={facebookIcon} title={'Facebook'} info={'@Juicebar_Ubriachi'}/>
+                <InfoItem icon={instaIcon} title={'Instagram'} info={'@Juicebar.Illegal'}/>
+                <InfoItem icon={facebookIcon} title={'Facebook'} info={'Juicebar_Ubriachi'}/>
             </div>
         </div>
     )      
