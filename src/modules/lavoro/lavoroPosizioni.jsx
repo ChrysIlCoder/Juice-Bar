1605c1f524391148e957c11d4ce530dfde4aaf18
ChrysIlCoder <creepert11.creeper@gmail.com>
2023-09-27 17:17:36 +0200

    Finito lavoro

--- src/modules/lavoro/lavoroPosizioni.jsx
+++ src/modules/lavoro/lavoroPosizioni.jsx
@@ -5,6 +5,7 @@ import { Positions } from '../../utils/database'
 
 export default function LavoroPosizioni(){
     const [query, setQuery] = useState("")
+    const [selectedType, setSelectedType] = useState(Positions.services[0])
 
     const filterSearch = Object.keys(Positions.jobs).filter(item => {
         return item.toLowerCase().includes(query.toLowerCase())
@@ -13,22 +14,35 @@ export default function LavoroPosizioni(){
     return (
         <div className='jobs-list-container'>
             <div className='search-container'>
-                <input type="search" value={query} onChange={e=> setQuery(e.target.value)} placeholder='Cerca posizioni...'/>
-                <select name="Tipo di posizione">
+                <input type="search" value={query} onChange={e => setQuery(e.target.value)} placeholder='Cerca posizioni...'/>
+                <select name="Tipo di posizione" onChange={e => setSelectedType(e.target.value)}>
                     {Positions.services.map((type, index) =>(
                         <option value={type} key={index}>{type}</option>
                     ))}
                 </select>
             </div>
             <div className='jobs-positions-container'>
-                {filterSearch.map((job, index) => (
-                    <JobPositionItem
-                        title={Positions.jobs[job].title}
-                        desc={Positions.jobs[job].desc}
-                        hours={Positions.jobs[job].hours}
-                        key={index}
-                    />
-                ))}
+                {
+                    filterSearch.map((job, index) => {
+                        const jobType = Positions.jobs[job].type;
+                        if (
+                            (selectedType == Positions.services[0]) ||
+                            (selectedType == Positions.services[1] && jobType == "S") ||
+                            (selectedType == Positions.services[2] && jobType == "G") ||
+                            (selectedType == Positions.services[3] && jobType == "I")
+                        ) {
+                            return (
+                                <JobPositionItem 
+                                    title={Positions.jobs[job].title}
+                                    desc={Positions.jobs[job].desc}
+                                    hours={Positions.jobs[job].hours}
+                                    type={Positions.jobs[job].type}
+                                    key={index}
+                                />
+                            )
+                        } return null
+                    })
+                }
             </div>
         </div>
     )
--- src/utils/database.jsx
+++ src/utils/database.jsx
@@ -2,7 +2,15 @@ export const Positions = {
     services: [
         "Tutti",
         "Servizio Clienti",
-        "Gestione e Amministrazione"
+        "Gestione e Amministrazione",
+        "Informatica"
+    ],
+
+    type: [
+        "T",
+        "S",
+        "G",
+        "I"
     ],
 
     jobs: {
@@ -10,51 +18,61 @@ export const Positions = {
             title: "Barista notturno",
             desc: "Lavora come barista notturno per servire drink e cocktails ai clienti",
             hours: "01:00 - 06:00",
+            type: "S"
         },
         Cameriere: {
             title: "Cameriere",
             desc: "Servi ai clienti i drink e pietanze richieste e preparate dallo chef di fiducia",
             hours: "Mattina - Pomeriggio - Sera",
+            type: "S"
         },
         Barman: {
             title: "Barman",
             desc: "Crea, prepara e servi drink ai clienti facendoli ubricare per massimizzare i guadagni",
             hours: "Mattina - Pomeriggio - Sera",
+            type: "S"
         },
         StorageManager: {
             title: "Storage Manager",
             desc: "Gestici la prenotazione e consegna dei pacchi Juice Bar",
             hours: "9:00 - 17:00",
+            type: "G"
         },
         HeadMarketing: {
             title: "Head Marketing",
             desc: "Gestisci le pubblicità e strategie di marketing per far risaltare Juice bar",
             hours: "Orario d'ufficio",
+            type: "G"
         },
         Commercialista: {
             title: "Commercialista",
             desc: "Gestici la commercializzazione aziendale dal punto di vista economico",
             hours: "Chiamata o da accordare",
+            type: "G"
         },
         Fotografo: {
             title: "Fotografo",
             desc: "Scatta e modifica foto che siano all'altezza dell'altezza",
             hours: "Ingaggio",
+            type: "I"
         },
         Grafico: {
             title: "Grafico",
             desc: "Crea album, copertine, poster e altre grafiche per il logo Juice Bar",
             hours: "Orario d'ufficio",
+            type: "I"
         },
         AddettoAllePulizie: {
             title: "Addetto alla pulizia",
             desc: "Pulisci la merda lasciata dai clienti molto ma molto garbati... E ovviamente anche da noi",
             hours: "Mattina - Pomeriggio - Sera",
+            type: "G"
         },
         DoorManager: {
             title: "Door Manager",
             desc: "Apri tutte le porte dell'edificio, compreso il bagno",
             hours: "Non puoi più uscire",
+            type: "S"
         }
     }
 }
